72e071fc3738f04e7f428709c43f6ed7cdf72130
miguel jimenez <denkatalabeno@gmail.com>
2020-04-03 00:11:25 +0800

    added section button components

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,3 +5,18 @@ require("channels")
 import "bootstrap";
 
 
+// erase after demoing
+
+const sectionButtons = document.querySelectorAll('.button-section');
+
+const addActive = (event) => {
+  event.preventDefault();
+  event.target.classList.add('active');
+}
+
+
+sectionButtons.forEach((button) => {
+  button.addEventListener('click', addActive);
+})
+
+
